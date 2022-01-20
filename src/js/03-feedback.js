a80c6ea56a0821b7c3daf78eec0a28a4ca39fc42
Oksana Orlova <kseniyaorlova88@gmail.com>
2022-01-20 15:51:19 +0300

    Правки к дз 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,41 @@
 import throttle from "lodash.throttle";
-import { objData } from './module/objData';
-import { jsonParceWhereStorage } from './module/jsonParceWhereStorage';
 
 
 const formEl = document.querySelector ('.feedback-form');
 const inputEl = document.querySelector ('.feedback-form input');
 const textareaEl = document.querySelector ('.feedback-form textarea');
-const statusStorage = jsonParceWhereStorage("feedback-form-state")
 
-inputEl.addEventListener ('input', throttle(populateFormItem, 500));
-textareaEl.addEventListener ('input', throttle(populateFormItem, 500));
+formEl.addEventListener ('input', throttle(onFormInput, 500));
 formEl.addEventListener ('submit', onFormSubmit);
+populateFormItem();
 
-if(statusStorage){    
-    inputEl.value = statusStorage.email;                                             
-    textareaEl.value = statusStorage.message;
+
+
+function onFormInput(evt) {
+    const mail = evt.currentTarget.email.value;   
+    const text = evt.currentTarget.message.value; 
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify({email: mail, message: text}));
+    
+    
 }
-
-
 function onFormSubmit (evt) {
     evt.preventDefault();
     evt.currentTarget.reset();
+    console.log(localStorage.getItem("feedback-form-state"));
     localStorage.removeItem("feedback-form-state");
-    console.log(objData);
     
     
 }
 
-function populateFormItem(evt) {
-    objData[evt.target.name] = evt.target.value
-    objData[evt.target.name] = evt.target.value
-    localStorage.setItem("feedback-form-state", JSON.stringify(objData))
+function populateFormItem() {
+    const savedMessage = localStorage.getItem("feedback-form-state");
+    const itemValue = JSON.parse(savedMessage);
+
+    if (savedMessage) {
+    inputEl.value = itemValue.email;
+        textareaEl.value = itemValue.message;
+
+    }
     
 }
-
